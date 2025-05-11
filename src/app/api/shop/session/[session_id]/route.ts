b2c591c948fce95e841ecc988fbe86c1d61a020a
bashir5995 <bashirseye@gmail.com>
2025-05-11 03:55:19 +0100

    updated success pages

--- src/app/api/shop/session/[session_id]/route.ts
+++ src/app/api/shop/session/[session_id]/route.ts
@@ -1,23 +1,26 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2025-02-24.acacia",
 });
 
-export async function GET(_: Request, { params }: { params: { session_id: string } }) {
+export async function GET(req: NextRequest, context: { params: { session_id: string } }) {
+  const { session_id } = context.params;
+
   try {
-    const session = await stripe.checkout.sessions.retrieve(params.session_id, {
+    const session = await stripe.checkout.sessions.retrieve(session_id, {
       expand: ["line_items.data.price.product", "payment_intent"],
     });
 
-    const lineItems = await stripe.checkout.sessions.listLineItems(params.session_id, {
+    const lineItems = await stripe.checkout.sessions.listLineItems(session_id, {
       expand: ["data.price.product"],
     });
 
-    const metadata = session.payment_intent && typeof session.payment_intent !== "string"
-      ? session.payment_intent.metadata
-      : {};
+    const metadata =
+      session.payment_intent && typeof session.payment_intent !== "string"
+        ? session.payment_intent.metadata
+        : {};
 
     return NextResponse.json({
       customer_email: session.customer_email,
