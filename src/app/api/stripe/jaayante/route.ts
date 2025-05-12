6a5f93a3f7a10711307a16825be47194e22b7ca3
bashir5995 <bashirseye@gmail.com>
2025-05-12 06:50:05 +0100

    text corrected like name of Sheikh Ahmadu Bamba and more

--- src/app/api/stripe/jaayante/route.ts
+++ src/app/api/stripe/jaayante/route.ts
@@ -68,7 +68,7 @@ export async function POST(req: NextRequest) {
           quantity: 1,
         },
       ],
-      success_url: `${baseUrl}/success/jaayante?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${baseUrl}/donate/jaayante/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${baseUrl}/checkout/cancel`,
     });
 
